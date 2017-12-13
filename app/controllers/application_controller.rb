4942cbe59415f8fa4a8a2914745b27da11219f1e
Anthony Zamarro <anthony.zamarro@gmail.com>
2017-12-13 12:50:33 +0100

    uncomment login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  # before_action :authenticate_registration!
+  before_action :authenticate_registration!
   attr_reader :current_user
   helper_method :current_user, :user_signed_in?
 
