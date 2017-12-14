71d414d3387de005b67a671c8c42e1641c543ac1
Anthony Zamarro <anthony.zamarro@gmail.com>
2017-12-14 15:56:32 +0100

    add scroll to reports page

---
+++ app/assets/javascripts/scroll.js
@@ -0,0 +1,9 @@
+const browse = document.getElementById('browse');
+
+browse.addEventListener('click', (event) => {
+  window.scroll({
+    top: 660,
+    left: 0,
+    behavior: 'smooth'
+  });
+});
