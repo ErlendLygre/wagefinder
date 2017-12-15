65a229df562ecb1e59261f3c2a67c80d7e69c780
Erlend Lygre <erlend.tl2@gmail.com>
2017-12-15 11:11:37 +0100

    stuff

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,6 +1,6 @@
-const browse = document.getElementById('browse');
+var browse = document.getElementById('browse');
 
-browse.addEventListener('click', (event) => {
+browse.addEventListener('click', function(event) {
   window.scroll({
     top: 660,
     left: 0,
