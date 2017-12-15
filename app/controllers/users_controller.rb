763af1f7f869868b79f339a3672b6994a8b5857e
Erlend Lygre <erlend.tl2@gmail.com>
2017-12-15 14:50:21 +0100

    added company average

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,15 @@ class UsersController < ApplicationController
     @yr_10 = CalculatorService.new({wage: current_user.wage.wage,
                                    years: current_user.wage.years_worked,
                                    years_new: current_user.wage.years_worked + 10 }).new_wage.to_i
+    @company_avg = company_avg(current_user.wage.field)
+  end
+
+  def company_avg(field, company = current_user.wage.company)
+    company_wages = Wage.where(company: company)
+    company_wage_by_field = company_wages.select{ |wage| wage.field == field }
+    company_wage_by_field.map!(&:wage)
+    company_total = company_wage_by_field.inject(0){|sum,x| sum + x }
+    return company_total / company_wage_by_field.size
   end
 
   def show
--- app/models/wage.rb
+++ app/models/wage.rb
@@ -2,4 +2,6 @@ class Wage < ApplicationRecord
   belongs_to :user
   belongs_to :field
   belongs_to :company
+  validates :wage, presence: true, inclusion: 100..10000000
+  validates :years_worked, presence: true, inclusion: 1..80
 end
