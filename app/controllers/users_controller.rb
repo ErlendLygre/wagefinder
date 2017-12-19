345c7e49e63ef63ebdd0227780e1ab7b50b2c9d5
Erlend Lygre <erlend.tl2@gmail.com>
2017-12-19 16:38:23 +0100

    added tabs, changed backend calculations to models instead of controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
   def index
+    @current_page = 'user'
     @companies = Company.all
     @user = current_user.registration.email
     @wage = Wage.all
+    calc = CalculatorService.new({ wage: current_user.wage.field.avg_wage,
+                                   years: 17,
+                                   years_new: current_user.wage.years_worked })
+    @should_be_making = calc.new_wage.to_i
+
     @yr_5 = CalculatorService.new({wage: current_user.wage.wage,
                                    years: current_user.wage.years_worked,
                                    years_new: current_user.wage.years_worked + 5 })
@@ -12,18 +18,26 @@ class UsersController < ApplicationController
                                    years: current_user.wage.years_worked,
                                    years_new: current_user.wage.years_worked + 10 })
                                    .new_wage.to_i + inflation(10)
-
-    @company_avg = CompanyAverageService.new(current_user.wage.field, current_user.wage.company).company_avg
     @user_field = current_user.wage.field
+
+    if @should_be_making < current_user.wage.wage
+      @how_you_are_doing = "doing well"
+    else
+      @how_you_are_doing = "ask for more"
+    end
   end
 
   def inflation (years)
     ((current_user.wage.wage * 1.024**years) - current_user.wage.wage).to_i
   end
 
-  def show
+  def fields
+    @current_page = 'fields'
   end
 
-  def create
+  def companies
+    @current_page = 'companies'
+    @company_avg = CompanyAverageService.new(current_user.wage.field, current_user.wage.company).company_avg
+    @company_data = current_user.wage.field.companies_hash
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :wages, only: [ :create, :new ]
   post 'preview', to: 'wages#preview'
   get 'preview', to: 'pages#test'
+  get 'fields', to: 'users#fields'
+  get 'companies', to: 'users#companies'
   resources :users, only: [ :index, :create ]
   root to: 'pages#home'
 end
